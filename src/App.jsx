d439eeead6ad8b349c7b68b197fda7282e040673
Nahuel Gramajo <nahuelz08@gmail.com>
2021-12-07 10:57:08 -0300

    añadido ItemCount

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.css'
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemListContainer/ItemCount';
 
 function App() {
+
 	return (
 		<div className="App">
 			<NavBar />
 			<ItemListContainer greeting="Hola, Bienvenido a NG Games!" />
+			<ItemCount inicial={1} max={10} />
 		</div>
 	);
 }
---
+++ src/components/ItemListContainer/ItemCount.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+
+function ItemCount({inicial, max}) {
+
+    const [value, setValue] = useState(inicial)
+
+    // Sumar, no permite pasar las 10 unidades
+    const btnSumar = () => {
+        value < max ?  setValue(value + 1) : console.log("Stock maximo")
+    }
+
+    // Restar, no permite llegar a 0 unidades
+    const btnRestar = () => {
+        value > inicial ?   setValue(value - 1) : console.log("No puedes añadir 0 productos al carrito")
+    }
+
+    // Agregar al carrito, muestra por consola la cantidad añadida
+    const onAdd = () => {
+        console.log("Agregaste " + value + " al carrito!")
+        setValue(inicial)
+    }
+    
+    return (
+        <div>
+            <div className="d-flex justify-content-center align-items-center">
+                <button onClick={btnRestar} className="btn btn-dark">-</button>
+                <h3 className="m-5">{value}</h3>
+                <button onClick={btnSumar} className="btn btn-dark">+</button>
+            </div>
+            <div className="d-flex justify-content-center">
+                <button onClick={onAdd} className="btn btn-primary mt-3">Añadir Carrito</button>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount
